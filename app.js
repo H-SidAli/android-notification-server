1259c5b9c425b0cd3600af1a8ce77392e6618a1e
Sidali <ns_hadji@esi.dz>
2025-04-24 18:11:35 +0100

    FIXED TYPO

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const notificationRoutes = require("./routes/notificationRoutes");
+const notificationRoutes = require("./routes/notification.routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
